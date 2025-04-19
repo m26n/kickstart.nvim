edf1cc4c37626d47a4c762d0e31a04c6ff393fea
Maurice Bergerhausen <mbn@fastmail.de>
2025-04-19 20:53:11 +0200

    add prettierd

--- init.lua
+++ init.lua
@@ -708,6 +708,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -765,8 +766,17 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        json = { "prettierd" },
+        html = { "prettierd" },
+        css = { "prettierd" },
+        markdown = { "prettierd" },
+        yaml = { "prettierd" },
+      }
+    }
   },
 
   { -- Autocompletion
