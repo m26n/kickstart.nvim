7b5283381b46cbe5eeb94a2f7835ca9cfa1ed03d
Maurice Bergerhausen <mbn@fastmail.de>
2025-04-21 02:28:39 +0200

    increase timeout for conform to prettierd doesn't timeout

--- init.lua
+++ init.lua
@@ -783,7 +783,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 10000,
             lsp_format = 'fallback',
           }
         end
