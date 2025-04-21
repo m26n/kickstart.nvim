7b4f487c6ef9df0df14a4f3c97032accc291a7cd
Maurice Bergerhausen <mbn@fastmail.de>
2025-04-21 02:27:59 +0200

    cleaner descriptions for keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,12 +1,17 @@
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move 1 line up' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move 1 line down' })
+-- Move selected lines up/down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move 1 line down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move 1 line up' })
 
-vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Append next line to current line without losing the cursor position' })
+-- Join lines without losing cursor
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines keeping cursor' })
 
-vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Keep current search item centered' })
-vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Keep current search item centered' })
+-- Keep search terms centered
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next match centered' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Prev match centered' })
 
+-- Center screen on scroll
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down centered' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up centered' })
+
+-- Disable Q
 vim.keymap.set('n', 'Q', '<nop>', { desc = 'Just no.' })
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Center page on half page down' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center page on half page up' })
