b78350f6c2beecdbe3e262aaca70ed7d32ae6961
Maurice Bergerhausen <mbn@fastmail.de>
2025-05-02 01:48:09 +0200

    switch to github theme

--- init.lua
+++ init.lua
@@ -908,26 +908,13 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
-    lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('github-theme').setup {
-        styles = {
-          comments = { italic = true }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      require('github-theme').setup {}
       vim.cmd.colorscheme 'github_dark_default'
       vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#24292f' })
     end,
